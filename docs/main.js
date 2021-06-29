5f4a9a7dc5236d304ff67a7fdae7fca861b1ab7f
Maria Diete <2010738297@stud.fh-kufstein.ac.at>
2021-06-29 20:51:24 +0200

    zusammen

--- docs/main.js
+++ docs/main.js
@@ -102,3 +102,23 @@ function watchPosition() {
     navigator.geolocation.watchPosition(success, error, options);
 }
 watchPosition();
+
+let pwaPrompt;
+const prompt = document.querySelector('#pwa-install-prompt');
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    pwaPrompt = e;
+    prompt.classList.add('show');
+})
+
+prompt.addEventListener('click', function(event) {
+    if (event.target.dataset.id === 'pwa-install-y' && pwaPrompt) {
+        pwaPrompt.prompt();
+        pwaPrompt.userChoice.then(() => {
+            prompt.classList.remove('show');
+            pwaPrompt = null;
+        });
+    } else {
+        prompt.classList.remove('show');
+    }
+});
